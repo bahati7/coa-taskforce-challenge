720f4c14217e12fef6d8f217c4c1a05279bc0529
Bahati Philemon <bahatiphilmon8@gmail.com>
2024-06-07 12:49:26 +0200

    create function hasContiguousSubarraySum and Initialize variables to store current sum and starting index

---
+++ Challenges/arrayMap.js
@@ -0,0 +1,12 @@
+function hasContiguousSubarraySum(arr, target) {
+  // Initialize variables to store current sum and starting index
+  let currentSum = 0;
+  let start = 0;
+
+ 
+
+  // If no subarray is found, return false
+  return false;
+}
+
+// Test cases
